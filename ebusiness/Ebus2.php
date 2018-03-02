6aeffd87dc2e0b1b4d5afe17e1acf3d2685a4909
Anna Foley <117434434@umail.ucc.ie>
2018-03-02 21:50:15 +0000

    update

--- ebusiness/Ebus2.php
+++ ebusiness/Ebus2.php
@@ -9,6 +9,15 @@ session_start();
         <link rel="Stylesheet" href="Cloud_pages.css" type="text/css"/>
         <script type="text/javascript" src="ebus2_validator.js"></script>
 
+ <!--tab on the top of the page so can go back home  or on to the next page-->
+                <ul>
+            <li> <a href="..//homepage.html">Home</a></li>
+             <li> <a href="..//Cv/cv_page1.html">Cv</a></li>
+              <li> <a href="..//interests/sports.html">Interests(Sports)</a></li>
+            <li> <a href="AboutCloud.html"> About Clouds</a></li>
+            <li> <a href="EBus1.php">Select a product</a></li>
+       </ul>
+       
         <title> Enter Details</title>
         
         
@@ -26,9 +35,9 @@ session_start();
                        border-radius: 8px;
                        width: 250px;
                        position:absolute;
-                       top:490px;
-                       right:300px;
-                       left:460px;
+                       top:450px;
+                       right:320px;
+                       left:50px;
             }
             
             #valdiate{
@@ -38,9 +47,42 @@ session_start();
                        border-radius: 8px;
                        width: 250px;
                        position:absolute;
-                       top:450px;
-                       right:300px;
-                       left:460px;
+                       top:540px;
+                       right:360px;
+                       left:730px;
+            }
+            
+            
+            
+            
+            #billing{position:absolute;
+                    top:150px;
+                    right:500px;
+                    left:150px;
+                    width:400px;
+                    height:350px;
+                    border-style:double;
+            }
+            
+            #payment{ position:absolute;
+                        top:150px;
+                        right:150px;
+                        left:700px;
+                          width:400px;
+                    height:500px;
+                    border-style: double;
+            }
+            
+            #btn-confirm{   padding: 12px 28px;
+                       background-color: rgb(208, 83, 71);
+                       font-size: 12px;
+                       border-radius: 8px;
+                       width: 250px;
+                       position:absolute;
+                       top:280px;
+                       right:920px;
+                       left:70px;
+                
             }
         </style>
         
@@ -48,12 +90,79 @@ session_start();
     
     <body>
         
-        <h2>Please enter your payment details.</h2>
+        <h1>Checkout</h1>
         
             <br />
             
             
-            <form method="POST" action="EBus3.php">
+         
+<br/>
+                <form method="POST" action="EBus3.php">
+                    
+                    <div class="billing" id="billing">
+                <h3> Billing Address</h3>
+                   <!--email and name needed-->
+                <label for "FirstName">
+                   First Name
+                     </label>
+                <input tpye="text" name="user_name" id="user_name" placeholder="First Name" maxlength="30">
+                <br/>
+                
+                <label for "LastName">
+                    Last Name
+                </label>
+                <input type="text" name="lastname" id="lastname" placeholder="Last Name" maxlength="15">
+                
+                </br>
+                 
+                 <label for "address">
+                   Address
+                 </label>
+              
+                    <input type="text" name="address" id="address" placeholder="Address" maxlength="30">
+                    
+                    <br/>
+                    <label for "street">
+                        Street
+                    </label>
+                    <input type="text" name="street" id="street" placeholder="Street" maxlength="30">
+                    
+                    <br/>
+                    <label for "town">
+                        Town/City
+                    </label>
+                    <input type="text" name="town" id="town" placeholder="Town/City">
+                    <br/>
+                    <label for "counties">
+                    Counties
+                    </label>
+                    <input type="tex" name="counties" id="counties" placeholder="Countires" maxlength="20">
+                    
+                    <br/>
+                    <label for "email">
+                    E-mail Address
+                     </label>
+                <input type="text" name="user_email" id="user_email" placeholder="E-mail" maxlength="30">
+               <br/>
+               <label for="number">
+                   Mobile Number
+               </label>
+               <input type="number" name="mobile" id="mobile" placeholder="Mobile Number" maxlength="10">
+               
+               <br/>    
+               
+               <div class="btn-confirm" id="btn-confirm">
+               <button class="confirm" id="confirm" onClick="validatebooking()" >
+                   Confirm Billing Address    
+               </button>
+               </div>
+             </div>
+               
+               </form>
+               
+                  <form method="POST" action="EBus3.php">
+                      <div class="payment" id="payment">
+                <h3>Please enter your payment details.</h3>
                 
                 <!--radio buttons to which way to pay-->
                 <!--visa-->
@@ -67,38 +176,44 @@ session_start();
                     Paypal
                 </label>
                 <input type="radio" id="paypal" name="paypal">
+                <br/>
                 
-                <form method="POST" action="EBus3.php">
-                
-                   <!--email and name needed-->
-                <label for "FirstName">
-                   First Name
-                     </label>
-                <input tpye="text" name="user_name" id="user_name" placeholder="First Name" maxlength="30">
-                
-                <label for "LastName">
-                    Last Name
-                </label>
-                <input type="text" name="lastname" id="lastname" placeholder="Last Name" maxlength="15">
-                
-                </br>
-                 
-                 <label for "address">
-                   Address
-                 </label>
-                <label for "email">
-                    <input type="text" name="address" id="name" placeholder="Address" maxlength="30">
-                    
-                    <br/>
-                    <label for "street"
-                    
-                    E-mail Address
-                     </label>
-                <input type="text" name="user_email" id="user_email" placeholder="E-mail" maxlength="30">
+                <h4>Visa/Debit Card</h4>
+            
+               <label for "cardname">
+               Card Holder's Name
+               </label>
+               <input type="text" name="cardname" id="cardname" maxlength="30">
+               <br/>
                
-                
-                </br>
-                
+               <label for "cardnumber">
+                   Card Number
+               </label>
+               <input type="text" name="cardnumber" id="cardnumber" maxlength="20">
+               
+               <br/>
+               <label for "expiry-date">
+                   Expiry Date
+               </label>
+               <input type="date" name="expiry-date" id="expiry-date">
+               
+               
+              <br/>
+              <label for "ccv">
+                  CCV Number
+                  </label>
+                  <input type="number" name="ccv" id="ccv" maxlength="3">
+               
+               <br/>
+               <h4>Paypal</h4>
+              
+               <label for "username">
+                   Username
+               </label>
+               <input type="text" name="username" id="username">
+               <br/>
+               
+               <label for "pin"
                 <label for="user_pin">
                      PIN 
                 </label>
@@ -110,14 +225,15 @@ session_start();
                     Proceed with Purchase 
                 </button>
               </div>
-            
+            </div>
             </form>
               
                <br />
              <div class="valdiate" id="valdiate">
-            <button  class = "btn btn-dark" onClick="validateDetails()"> Validate </button>
+            <button  class = "btn btn-dark"  id="btnvalidate"   disabled onClick="validateDetails()">
+                 Validate </button>
                   </div>
-       
+      
         
       <!--set session variables-->
       
--- ebusiness/ebus2_validator.js
+++ ebusiness/ebus2_validator.js
@@ -1,14 +1,65 @@
 /* global $ */
 
+function validatebooking(){
+ //delcaring variables
+ var last;
+ var name;
+ var address
+ var street;
+ var county;
+   var email;
+   var number;
+  var town;
+   //assigning values to the variables
+    name = document.getElementById("user_name").value;
+   email= document.getElementById("user_email").value;
+   last = document.getElementById("lastname").value;
+   address = document.getElementById("address").value;
+   street = document.getElementById("street").value;
+   county = document.getElementById("counties").value;
+ number = document.getElementById("mobile").value;
+ town = document.getElementById("town").value
+ 
+ //if statement to ensure all the correct details for billing are filled in
+  if (name==""){
+        alert("Please enter your Name");
+    }
+       
+    else if (email==""){
+        alert("Please enter your E-mail Address");
+    }
+    else if (last==""){
+        alert("Please enter your last name");
+    }
+    else if (address==""){
+        alert("Please enter your address");
+    }
+    else if (street==""){
+        alert("Please enter your street address");
+    }
+    else if (town==""){
+        alert("Please enter your Town/city");
+    }
+    else if (county==""){
+    alert("Please enter your county");
+    }
+    else if (number==""){
+        alert("Please enter your mobile number");
+    }
+ else {
+       enablebtnvalidate();
+    }
+}
+    function enablebtnvalidate(){
+     $('#btnvalidate').prop('disabled',false);
+}
 function validateDetails(){
-//declaring the variables 
     var pin;
-   var name;
-     var email;
+   
+   
 //assigning values to the variables 
     pin = document.getElementById("user_pin").value;
-        name = document.getElementById("user_name").value;
-   email= document.getElementById("user_email").value;
+       
    
 //if statement to ensure all details are entered correct before carrying on 
     if (pin == ""){
@@ -17,16 +68,6 @@ function validateDetails(){
         }
     else if (String(pin).length<4){
         alert("Please make sure your PIN is accurate");
-       
-        }
- 
-    else if (name==""){
-        alert("Please enter your Name");
-    }
-       
-    else if (email==""){
-        alert("Please enter your E-mail Address");
-        
         
     }
     else {
